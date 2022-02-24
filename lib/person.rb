ef21b9238b0016b39fec319a685f5426c626f7be
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-24 10:43:30 -0800

    commit tree 027d6ccb1af220426bca51d3136bc64fabe709c0

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
+    def job=(job)
+        @job = job
+    end
 
+    def job
+        @job
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
 end
+
+kanye = Person.new
+kanye.job=("Kanye")
+kanye.job
